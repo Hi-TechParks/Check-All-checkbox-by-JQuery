8b909ce40b35ef5508ebb24d3a7e6628543ab3c0
Md Habibur Rahman <mhrahman@biziitech.com>
2019-01-09 15:03:30 +0600

    Update with comments

--- check-uncheck-all.js
+++ check-uncheck-all.js
@@ -1,18 +1,20 @@
+// checkbox selector
 $("input[name=all_flag]").click(function(){
     if($(this).is(":checked")){
-
+        // check all checkbox
         $("input[name=vedio_flag]").prop('checked', true);
         $("input[name=audio_flag]").prop('checked', true);
 
     }
     else if($(this).is(":not(:checked)")){
-
+        // uncheck all checkbox
         $("input[name=vedio_flag]").prop('checked', false);
         $("input[name=audio_flag]").prop('checked', false);
 
     }
 });
 
+// uncheck all-checkbox if anyone checkbox get uncheck
 $("input[name=vedio_flag], input[name=audio_flag]").click(function(){
     if($(this).is(":not(:checked)")){
 
